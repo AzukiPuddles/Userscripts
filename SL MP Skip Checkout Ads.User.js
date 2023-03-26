87057311c8221ccc257ee2a9d86137d2909a3069
AzukiPuddles <Azuki@Pudl.es>
2023-03-25 22:29:28 -0400

    Update

--- SL MP Skip Checkout Ads.User.js
+++ SL MP Skip Checkout Ads.User.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         SL MP Skip Checkout Ads
-// @namespace    https://Pudl.es/Contact
 // @version      0.1
 // @description  Automatically skips the ad page on checkout when you're only buying free stuff. Used to be tolerable before, but now they force you to scroll through 96 items, this is regardless of membership as well.
 // @author       AzukiPuddles#0001
 // @match        https://marketplace.secondlife.com/orders/checkout/continue
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=secondlife.com
-// @grant        none
+// @updateURL    https://github.com/AzukiPuddles/Userscripts/blob/master/SL%20MP%20Skip%20Checkout%20Ads.User.js
+// @license      MIT
 // ==/UserScript==
 
 (function () {
