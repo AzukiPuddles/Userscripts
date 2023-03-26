0a55def78a086427d72762e871d3e7ecfcaae5eb
AzukiPuddles <Azuki@Pudl.es>
2023-03-25 22:14:23 -0400

    Initial commit

---
+++ SL MP Skip Checkout Ads.User.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         SL MP Skip Checkout Ads
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Automatically skips the ad page on checkout when you're only buying free stuff. Used to be tolerable before, but now they force you to scroll through 96 items, this is regardless of membership as well.
+// @author       AzukiPuddles#0001
+// @match        https://marketplace.secondlife.com/orders/checkout/continue
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=secondlife.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  document.getElementsByClassName("cart-button")[0].click();
+})();
